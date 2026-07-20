48b986cb4ba7dde884c07260cc72b1a71f8d2aff
andersonsimplicio <anderson25eu@gmail.com>
2026-07-20 18:45:21 -0300

    Animação

--- Assets/Scenes/Script/Player.cs
+++ Assets/Scenes/Script/Player.cs
@@ -5,33 +5,40 @@ public class Player : MonoBehaviour
 {
     public int life = 40;
     public float speed = 5f;
+    private Vector2 direction;
+
+    public Vector2 _direction
+    {
+        get { return this.direction;} 
+        set { this.direction = value;} 
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Inicio da Cena");
+        
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = Vector2.zero;
-            Debug.Log($"Inicio da Cena {direction}");
-         transform.position += Mover(direction) * (speed * Time.deltaTime);
-      
+            Vector2 direction = Vector2.zero;
+            transform.position += (Vector3)Mover(direction) * (speed * Time.deltaTime);     
     }
-
+    
     Vector3 Mover(Vector2 direction)
     {
-        direction = Vector2.zero;
+        this.direction = Vector2.zero;
         if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) 
-            direction.y = 1f;
+            this.direction.y = 1f;
         if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) 
-            direction.y = -1f;
+            this.direction.y = -1f;
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) 
-            direction.x = -1f;
+            this.direction.x = -1f;
         if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) 
-            direction.x = 1f;
-        return (Vector3)direction.normalized;
+            this.direction.x = 1f;
+        return (Vector3)this.direction.normalized;
     }
 }
---
+++ Assets/Scenes/Script/PlayerAnime.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerAnime : MonoBehaviour
+{
+    private Player player;
+    private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GetComponent<Player>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        if (player._direction.sqrMagnitude > 0){             
+            this.animator.SetInteger("transicao", 1);
+        }else{
+            this.animator.SetInteger("transicao", 0);
+        }
+
+        if (player._direction.x > 0){             
+          transform.eulerAngles = new Vector2(0,0);
+        }else if (player._direction.x < 0){
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+    }
+}
